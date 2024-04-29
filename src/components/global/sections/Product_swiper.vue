aadd0bd2457905c05548cb0df201b0d1c57623f6
Kirill <zahvatinkirill@mail.ru>
2024-04-30 00:24:56 +0300

    фича: начал создавать свайпер

---
+++ src/components/global/sections/Product_swiper.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Product_card from "../components/Product_card.vue";
+const params1 = ["Драм-картридж ФРОМБОАРД s-Line DK-1150 для Kyocera ECOSYS P2235, P2335","DFKYEP2235040",
+  100000,"3 656 ₽","ХИТ","-5%"]
+//"/src/public/1card.png",
+
+</script>
+
+<template>
+  <Product_card :name="params1[0]" :article="params1[1]" :resource="params1[2]" :price="params1[3]"
+                :hit="params1[4]" :discount="params1[5]"> </Product_card>
+</template>
+
+<style scoped>
+
+</style>
