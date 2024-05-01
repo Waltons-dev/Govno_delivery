11d611836ed12ded51fc1328d478ac7e1f3f02b6
Kirill <zahvatinkirill@mail.ru>
2024-05-02 00:11:49 +0300

    fix: решена проблема с приемом свойств

--- src/components/global/components/Product_card.vue
+++ src/components/global/components/Product_card.vue
@@ -1,34 +1,25 @@
 <script setup>
 
-const props = defineProps(["src","name","article","resource","price","hit","discount"])
-const card_hit = document.getElementById('card__hit');
-const card_discount = document.getElementById('card__discount');
-if ("hit"==="ХИТ"){
-  card_hit.display="inline-block";
-}
-if ("discount"===""){
-  card_discount.display="inline-block";
-}
-console.log(card_hit,card_discount)//почему не находит элементы???
+const props = defineProps(["src","name","vendor","resource","price","hit","discount"])
 </script>
 
 <template>
     <div class="card">
-      <img class="card__image" src="/src/public/1card.png" alt="Фото драм-картриджа"> <!--не могу поставить в src константу-->
+      <img class="card__image" :src=src alt="Фото драм-картриджа">
       <div class="card__additional_info">
-        <span id="card__hit">{{hit}}</span>
-        <span id="card__discount">{{discount}}</span>
+        <span v-if="hit" id="card__hit">ХИТ</span>
+        <span v-if="discount" id="card__discount">{{discount}}</span>
       </div>
 
       <p class="card__name">{{name}}</p>
       <div class="card__info">
-        <span class="info-item info-main-text">Артикул:<span class="info-sub-text">{{article}}</span></span>
+        <span class="info-item info-main-text">Артикул:<span class="info-sub-text">{{vendor}}</span></span>
         <span class="info-item info-main-text">Цвет:
           <span class="info-sub-text">
             Черный
             <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="7" cy="7" r="7" fill="black"/>
-</svg>
+              <circle cx="7" cy="7" r="7" fill="black"/>
+            </svg>
           </span>
         </span>
         <span class="info-item info-main-text">Ресурс: <span class="info-sub-text">{{resource}}</span></span>
@@ -58,12 +49,13 @@ console.log(card_hit,card_discount)//почему не находит элеме
   display: flex;
   flex-direction: column;
   gap: 20px;
-  max-width: 344px;
   max-height: 611px;
   border-radius: 2px;
   border: 1px solid #D7D7D766;
+  background-color: white;
 }
 .card__image{
+  align-self: center;
   max-width: 281px;
   max-height: 281px;
   margin-top: 20px;
@@ -74,7 +66,6 @@ console.log(card_hit,card_discount)//почему не находит элеме
   margin-top: 20px;
   display: flex;
   gap: 4px;
-
 }
 #card__hit{
   font-size: 12px;
@@ -86,7 +77,6 @@ console.log(card_hit,card_discount)//почему не находит элеме
   height: 26px;
   border-radius: 2px;
   align-content: center;
-  display: none;
 }
 #card__discount{
   font-size: 16px;
@@ -98,8 +88,6 @@ console.log(card_hit,card_discount)//почему не находит элеме
   height: 26px;
   border-radius: 2px;
   align-content: center;
-  display: none;
-
 }
 .card__name{
   font-size: 18px;
@@ -143,6 +131,7 @@ console.log(card_hit,card_discount)//почему не находит элеме
 .card__actions{
   display: flex;
   justify-content: space-evenly;
+  margin-bottom: 20px;
 }
 .add-delete-button{
   width: 142px;
