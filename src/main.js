268268658ff711b5c364f9095e0735ffc3111e40
Kirill <zahvatinkirill@mail.ru>
2024-05-02 00:02:47 +0300

    фича: подключен unhead

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import globalComponents from "./components/global"
+import {createHead} from "@unhead/vue";
 
 const app = createApp(App)
+const head = createHead()
 app.use(globalComponents)
 
-app.mount("#app")
+app.use(head).mount("#app")
