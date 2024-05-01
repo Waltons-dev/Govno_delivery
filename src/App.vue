2592e56df41864c3eef4e32d057f97439d7db465
Kirill <zahvatinkirill@mail.ru>
2024-05-02 00:06:01 +0300

    фича: создана секция с каталогом и подбором картриджей

--- src/App.vue
+++ src/App.vue
@@ -26,14 +26,34 @@ const swiper_names = {name1:"Хиты продаж", name2:"Новинки", nam
 
 <template>
   <Header></Header>
-  <Product_swiper></Product_swiper>
-  <Catalogue></Catalogue>
+  <section class="filer-catalogue _container">
+    <Filter class="filer-catalogue__filter"></Filter>
+    <Catalogue class="filer-catalogue__catalogue"></Catalogue>
+  </section>
+
+  <Product_swiper :swiper_name=swiper_names.name1></Product_swiper>
+  <Product_swiper :swiper_name=swiper_names.name2></Product_swiper>
+  <Product_swiper :swiper_name=swiper_names.name3></Product_swiper>
+
 </template>
 
-<style scoped>
-*{margin: 0; padding: 0; border: 0; box-sizing: border-box; font-family: "Lato", serif;
-  @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');}
-body{
-
+<style lang="scss">
+._container{
+  max-width: 1800px;
+  margin: 0 auto;
+  padding: 0 15px;
 }
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  box-sizing: border-box;
+  font-family:  "Lato", sans-serif;
+}
+.filer-catalogue{
+  display: flex;
+  margin-bottom: 40px;
+  gap: 20px;
+}
+
 </style>
