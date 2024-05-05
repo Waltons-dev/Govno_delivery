23a59fd7ce08947ce556f6e44721225d22e2d41f
Kirill <zahvatinkirill@mail.ru>
2024-05-05 19:37:02 +0300

    фича: начата разработка блока sub_hero

---
+++ src/components/global/components/Sub_Hero_card.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <article class="sub_hero_card">
+    <div class="sub_hero_card__container">
+      <div class="sub_hero_card__img">
+
+      </div>
+      <div class="sub_hero_card__text">
+
+      </div>
+    </div>
+  </article>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/global/sections/Sub_Hero.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import Sub_Hero_card from "../components/Sub_Hero_card.vue";
+</script>
+
+<template>
+  <Sub_Hero_card></Sub_Hero_card>
+</template>
+
+<style scoped>
+
+</style>
